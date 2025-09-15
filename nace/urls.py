867f3bd2fafa8adde7f8ed6ce3e721098bd97475
Slade <sladephantom60@gmail.com>
2025-09-15 16:34:35 +0300

    third commit

--- nace/urls.py
+++ nace/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('destination.html', views.destination, name='destination'),
+    path('discount.html', views.discount, name='discount'),
+    path('about.html', views.about, name='about'),
+    path('blog.html', views.blog, name='blog'),
+    path('contact.html', views.contact, name='contact'),
+    path('booking.html', views.booking, name='booking'),
+
+
     
 ]
